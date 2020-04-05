6b6213083d23e3588fdac408aedb50da799a6cc9
Marko Tribl <triblmarko@gmail.com>
2020-04-05 15:53:43 +0200

    Add 'CoreDataFeedStore' data model and its model representation (ManagedCache and ManagedFeedImage)

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     
@@ -25,4 +26,22 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     
+}
+
+// MARK: Core Data Models
+
+@objc(ManagedCache)
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+
+@objc(ManagedFeedImage)
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
